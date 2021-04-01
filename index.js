cce8890b2b73e8b6ba6063768a825b3b33199dca
marianaRodLima <marianarlima93@gmail.com>
2021-04-01 12:10:42 -0300

    Aula 01/04

--- index.js
+++ index.js
@@ -55,23 +55,50 @@ app.post("/projects", (request, response) => {
 });
 
 app.put("/projects/:id", (request, response) => {
-    const params = request.params;
-    console.log(params)
-    return response.json ([
-        "Projeto 50",
+    const { id } = request.params; //pegamos nosso id
+    const  {title, owner} = request.body; //retornando uma nova informação
+
+    
+    const projectIndex = projects.findIndex(project => project.id === id);
+
+    if (projectIndex < 0) {
+        return response.status(400).json({ error: "Projeto não encontrado" });
+    }
+
+    const project = {
+        id,
+        title,
+        owner,
+    }
+
+    projects[projectIndex] = project;
+    
+    //console.log(params)
+    return response.json (project)
+      /*  "Projeto 50",
         "Projeto 2",
         "Projeto 3",
         "Projeto 4",
-        "Projeto 5"
-    ])
+        "Projeto 5"*/
+    
 
 });
 
 app.delete("/projects/:id", (request, response) => {
-    return response.json([
-        "Projeto 50",
-        "Projeto 2"
-    ])
+    const { id } = request.params;
+    
+    const projectIndex = projects.findIndex(project => project.id === id);
+
+    if (projectIndex < 0) {
+        return response.status(400).json({ error: "Projeto não encontrado" });
+    }
+
+        projects.splice(projectIndex, 1);
+
+    return response.status(204).send();
+        //"Projeto 50",
+       // "Projeto 2"
+    //])
 })
 
 app.listen(3000) //, () => {
